6ff7575d231942113cd95c968c5c989af3298b57
Gabriela Akemi Shima <gabrielashima@gmail.com>
2018-03-09 23:47:17 -0300

    quase lá
     Palavra12! 5! 3!

--- src/main.c
+++ src/main.c
@@ -8,27 +8,27 @@
 
 #define TAMANHO_MAX 50
 
-double str2float (char *str);
-double fatorial(float num);
+float str2float (char *str);
+float fatorial(float num);
 
 int main () {
 
-  char string[TAMANHO_MAX], num[TAMANHO_MAX], aux = '0';
+  char string[TAMANHO_MAX], num[TAMANHO_MAX];
   int i = 0, j = 0, k = 0;
   float fat, somatorio = 0, numeros_convertidos[TAMANHO_MAX];
   int estado = 0;
 
-  printf ("-------------------------\n");
-  printf("Entre com a string: ");
+  // printf ("-------------------------\n");
+  // printf("Entre com a string: ");
   fgets (string, TAMANHO_MAX, stdin);
-  printf ("string lida: %s \n", string);
-  printf ("-------------------------\n");
+  //printf ("string lida: %s \n", string);
+  //printf ("-------------------------\n");
 
   while(string[i] != '\0') {
     switch (estado) {
 
       case 0 :
-        printf("Estado (0) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
+        //printf("Estado (0) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
         if(string[i] == ' ') {
           estado = 0;
         }
@@ -45,7 +45,7 @@ int main () {
       break;
 
       case 1 :
-        printf("Estado (1) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
+        //printf("Estado (1) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
         if(string[i] == ' ' || string[i] == '\n' || string[i] == '\0') {
           estado = 2;
           num[j] = string[i-1];
@@ -53,7 +53,7 @@ int main () {
           num[j] = '\0';
           numeros_convertidos[k] = str2float(num); //transforma [char*]num em float
           k++;
-          printf("num[%d] = %s foi armazenado em numeros_convertidos[%d] = %.2f\n", j-1, num, k-1, numeros_convertidos[k-1]);
+          //printf("num[%d] = %s foi armazenado em numeros_convertidos[%d] = %.2f\n", j-1, num, k-1, numeros_convertidos[k-1]);
           for(int g = j ; g >= 0; --g) { //limpa o vetor auxiliar [char*]num
             num[g] = 0;
           }
@@ -81,7 +81,7 @@ int main () {
       break;
 
       case 2 :
-        printf("Estado (2) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
+        //printf("Estado (2) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
         if(string[i] == ' ' || string[i] == '\n' || string[i] == '\0') {
           estado = 0;
         }
@@ -98,7 +98,7 @@ int main () {
       break;
 
       case 3 :
-        printf("Estado (3) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
+        //printf("Estado (3) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
         if(string[i] == ' ' || string[i] == '\n' || string[i] == '\0') {
           estado = 0;
         }
@@ -112,10 +112,15 @@ int main () {
       break;
 
       case 4:
-        printf("Estado (4) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
+        //printf("Estado (4) - estado (%d)  |  string[i] = %c\n", estado, string[i]);
         if(string[i] == ' ' || string[i] == '\n' || string[i] == '\0') {
           estado = 2;
+          //printf("numeros_convertidos[%d] = %.2f  |  fatorial: %.2f\n", k-1,numeros_convertidos[k-1], fatorial(numeros_convertidos[k-1]) );
           numeros_convertidos[k-1] = fatorial(numeros_convertidos[k-1]);
+          for(int g = j ; g >= 0; --g) { //limpa o vetor auxiliar [char*]num
+            num[g] = 0;
+          }
+          j = 0;
         }
         else  {
           estado = 3;
@@ -133,19 +138,19 @@ int main () {
     somatorio = somatorio + numeros_convertidos[g];
   }
   printf("%.2f\n", somatorio);
-  printf ("\n-------------------------\n");
-  printf("Fim do programa!\n");
+  //printf ("\n-------------------------\n");
+  //printf("Fim do programa!\n");
 
   return 0;
 
 }
 
-double str2float (char *num) {
+float str2float (char *num) {
   if (!num || !*num) {
      return 0;
    }
-     double integerPart = 0;
-     double fractionPart = 0;
+     float integerPart = 0;
+     float fractionPart = 0;
      int divisorForFraction = 1;
      int sign = 1;
      bool inFraction = false;
@@ -190,8 +195,8 @@ double str2float (char *num) {
      return sign * (integerPart + fractionPart/divisorForFraction);
 }
 
-double fatorial(float num) {
-  double fat = 1;
+float fatorial(float num) {
+  float fat = 1;
   for(int i = 1; i <= num; ++i) {
     fat = fat*i;
   }
